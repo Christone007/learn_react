ea6ac1b9292253ee553bf84df486f71895e30e63
Christone007 <exellentemy@gmail.com>
2025-09-20 11:26:26 +0100

    Complete static page without styling

--- static_react_page/src/main.jsx
+++ static_react_page/src/main.jsx
@@ -1,12 +1,22 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <>
-    <ul>
-      <li>React</li>
-      <li>Is</li>
-      <li>Awesome</li>
-    </ul>
-
+    <div>
+      <img src="/src/assets/react.svg" width="40px" alt="React logo" />
+      <h1>ReactFacts</h1>
+    </div>
+    <div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise aps, including mobile apps</li>
+      </ul>
+      <img src="/src/assets/react.svg" />
+    </div>
   </>
-)
+);
