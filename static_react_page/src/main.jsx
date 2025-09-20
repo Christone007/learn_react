cd46190619fb2a781a112b76375bed54eb33bf9e
Christone007 <exellentemy@gmail.com>
2025-09-20 08:19:58 +0100

    initialize project

---
+++ static_react_page/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root')).render(
+  <>
+    <ul>
+      <li>React</li>
+      <li>Is</li>
+      <li>Awesome</li>
+    </ul>
+
+  </>
+)
