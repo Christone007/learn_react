c38581cb6e5ab6520bc6a3a2909dafeeaffae382
Christone007 <exellentemy@gmail.com>
2025-09-21 17:32:44 +0100

    Create the Entry component and style it

--- 03-travel_journal/src/App.jsx
+++ 03-travel_journal/src/App.jsx
@@ -1,5 +1,11 @@
 import Header from "/src/components/Header";
+import Entry from "/src/components/Entry";
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Entry />
+    </>
+  );
 }
