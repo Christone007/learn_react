a9639180f3b5332653f6c05f1f4d0eb578d45853
Christone007 <exellentemy@gmail.com>
2025-09-21 10:49:28 +0100

    Create the react facts project structure

---
+++ 02-react_facts/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <nav className="nav">
+        <img
+          src="/src/assets/react.svg"
+          className="nav-logo"
+          alt="React Logo"
+        />
+        <span className="nav-heading">React Facts</span>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
