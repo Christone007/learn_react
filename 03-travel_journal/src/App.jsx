ab30840b6fba31b026212a4ce0f5dbf60ba3e3d7
Christone007 <exellentemy@gmail.com>
2025-09-21 15:54:20 +0100

    Create a Header component

---
+++ 03-travel_journal/src/App.jsx
@@ -0,0 +1,5 @@
+import Header from "/src/components/Header";
+
+export default function App() {
+  return <Header />;
+}
---
+++ 03-travel_journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <img src="/public/globe.png" className="nav-img" alt="Globe Logo" />
+        <span className="nav-title">my travel journal.</span>
+      </nav>
+    </header>
+  );
+}
