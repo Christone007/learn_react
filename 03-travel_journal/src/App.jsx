45f85a27d2fab6cdca4a9313a969efdd659c2cbb
Christone007 <exellentemy@gmail.com>
2025-09-25 16:59:02 +0100

    Use props to send data to Entry component

--- 03-travel_journal/src/App.jsx
+++ 03-travel_journal/src/App.jsx
@@ -5,9 +5,17 @@ export default function App() {
   return (
     <>
       <Header />
-      <Entry name="Emeka" age="27" occupation="doctor" />
-      <Entry name="Salome" age="24" occupation="student" />
-      <Entry name="Nonye" age="22" occupation="student" />
+      <Entry
+        img={{
+          src: "https://scrimba.com/links/travel-journal-japan-image-url",
+          alt: "Mount Fuji",
+        }}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      />
     </>
   );
 }
--- 03-travel_journal/src/components/Entry.jsx
+++ 03-travel_journal/src/components/Entry.jsx
@@ -1,35 +1,24 @@
-export default function Entry(props) {
-  console.log(props);
+import markerIcon from "/src/assets/marker.png";
 
+export default function Entry(props) {
   return (
     <div className="entry-card">
-      <img
-        src="https://scrimba.com/links/travel-journal-japan-image-url"
-        className="entry-img"
-        alt="Japan"
-      />
+      <img src={props.img.src} className="entry-img" alt={props.img.alt} />
       <div className="entry-content">
         <div className="entry-location">
           <img
-            src="/public/marker.png"
+            src={markerIcon}
             className="entry-location-icon"
             alt="location icon"
           />
-          <span className="entry-location-name">japan</span>
-          <a
-            href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-            className="entry-location-link"
-          >
+          <span className="entry-location-name">{props.country}</span>
+          <a href={props.googleMapsLink} className="entry-location-link">
             View on Google Maps
           </a>
         </div>
-        <h1 className="entry-title">Mount Fuji</h1>
-        <span className="entry-date">12 Jan, 2021 - 24 Jan, 2021</span>
-        <p className="entry-description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h1 className="entry-title">{props.title}</h1>
+        <span className="entry-date">{props.dates}</span>
+        <p className="entry-description">{props.text}</p>
       </div>
     </div>
   );
