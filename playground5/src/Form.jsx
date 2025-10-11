8c26ea1039c1b191d21d3fcf77ae1034a0cc2e08
Christone007 <exellentemy@gmail.com>
2025-10-11 05:14:12 +0100

    React forms

--- playground5/src/Form.jsx
+++ playground5/src/Form.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 export default function Form() {
+  function signIn(formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(password, email);
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form action="">
+      <form onSubmit={signIn}>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           id="email"
           name="email"
           placeholder="example@email.com"
+          autoComplete="example@email.com"
         />
         <br />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          autoComplete="pass"
+        />
+        <br />
+
+        <button>Submit</button>
       </form>
     </section>
   );
